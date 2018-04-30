140a42e1d1301a25d58631f16569f959c538a80a
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-30 16:37:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,6 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-  
 
     if req.path.match(/items/)
     item_name = req.path.split("/items/").last
