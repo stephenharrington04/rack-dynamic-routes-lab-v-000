2c9ae295b54a54fcdffe01241421883bf215cfdf
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-30 16:45:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,5 @@
 class Application
-  @@items = [Item.new("Figs", 3.42), Item.new("Pears", 0.99)]
+#  @@items = [Item.new("Figs", 3.42), Item.new("Pears", 0.99)]
 
   def call(env)
     resp = Rack::Response.new
