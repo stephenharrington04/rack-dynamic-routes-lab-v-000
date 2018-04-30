1cd9a9cc16c8e7f8c7a06083d7dd006367da0732
Stephen Harrington <stephenharrington04@yahoo.com>
2018-04-30 16:36:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,16 @@ class Application
   end
 
   if req.path.match(/items/)
-
+    item_name = req.path.split("/items/").last
+    if item = @@item.find{|i| i.name == item_name}
+      resp.write item.price
+    else
+      resp.status = 400
+      resp.write "Item not found"
+    end
+  else
+    resp.status = 400
+    resp.write "Item not found"
+  end
 
 end
